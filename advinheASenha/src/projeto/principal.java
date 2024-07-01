d78cbb5a5c965c6e2958d49bc4ea74fd88c32076
Luan <luancortes208@gmail.com>
2024-07-01 16:55:01 -0300

    Estrutura while

---
+++ advinheASenha/src/projeto/principal.java
@@ -0,0 +1,29 @@
+package projeto;
+
+import java.util.Scanner;
+
+public class principal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int senha = 2002;
+		
+		System.out.println("Este programa tem como objetivo advinhar qual é a senha correta.");
+		System.out.print("Portanto, advinhe a senha: ");
+		
+		int advinha = sc.nextInt();
+		
+		while (advinha != senha) {
+			System.out.println("Senha inválida");
+			System.out.print("Advinhe a senha: ");
+			advinha = sc.nextInt();
+		}
+		if (advinha == senha ) {
+			System.out.println("Acesso permitido");
+		}
+		
+		sc.close();
+	}
+
+}
